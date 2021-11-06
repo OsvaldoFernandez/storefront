e91280d2266daee333f96dcb9c8584a58a40d98a
OsvaldoFernandez <osvaldo.g.fernandez@gmail.com>
2021-11-05 23:29:18 -0300

    added users to API (#3)

--- store/models.py
+++ store/models.py
@@ -1,8 +1,11 @@
+from django.contrib import admin
+from django.conf import settings
 from django.core import validators
 from django.db import models
 from django.db.models.deletion import CASCADE
 from django.db.models.fields import CharField, related
 from django.core.validators import MinValueValidator
+
 from uuid import uuid4
 
 # Create your models here.
@@ -52,19 +55,28 @@ class Customer(models.Model):
     MEMBERSHIP_SILVER = 'S'
     MEMBERSHIP_GOLD = 'G'
     MEMBERSHIP_CHOICES = [
-        (MEMBERSHIP_BRONCE, 'Bronce'),
+        (MEMBERSHIP_BRONCE, 'Bronze'),
         (MEMBERSHIP_SILVER, 'Silver'),
         (MEMBERSHIP_GOLD, 'Gold')
     ]
-    first_name = models.CharField(max_length=255)
-    last_name = models.CharField(max_length=255)
-    email = models.EmailField(unique=True)
     phone = models.CharField(max_length=255)
-    birth_date = models.DateField(null=True)
+    birth_date = models.DateField(null=True, blank=True)
     membership = models.CharField(max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONCE)
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=CASCADE)
 
     def __str__(self) -> str:
-        return f'{self.first_name} {self.last_name}'
+        return f'{self.user.first_name} {self.user.last_name}'
+
+    @admin.display(ordering = 'user__first_name')
+    def first_name(self):
+        self.user.first_name
+
+    @admin.display(ordering = 'user__last_name')
+    def last_name(self):
+        self.user.last_name
+
+    class Meta:
+        ordering = ['user__first_name', 'user__last_name']
     
 
 class Order(models.Model):
@@ -79,6 +91,11 @@ class Order(models.Model):
     placed_at = models.DateTimeField(auto_now_add=True)
     payment_status = models.CharField(max_length=1, choices=PAYMENT_STATUS_CHOICES, default=PAYMENT_STATUS_PENDING)
     customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
+
+    class Meta: 
+        permissions = [
+            ('cancel_order', 'Can cancel order')
+        ]
 
 class OrderItem(models.Model):
     product = models.ForeignKey(Product, on_delete=models.PROTECT, related_name='orderitems')
